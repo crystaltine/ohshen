4c53119614c31dda08cfa5921e3f83bfc39a5b2f
daniel360kim <nmrs.thrust@gmail.com>
2023-06-14 13:24:41 -0600

    added gpu and cpu compatibility

--- heatmap2d.py
+++ heatmap2d.py
@@ -16,8 +16,10 @@ import matplotlib.colors as colors
 
 if torch.cuda.is_available():
     print("CUDA is available, using GPU")
+    device = torch.device("cuda")
 else:
     print("CUDA is not available, using CPU")
+    device = torch.device("cpu")
 
 #constants
 LENGTH = 100
@@ -44,13 +46,16 @@ heatmap_random.heat_square((90, 90), RADIUS, 0.5)
 heatmap_random = heatmap_random.get_iterable()
 
 def runtimestep(heatmap: torch.Tensor, time_counter):
+    # Move tensors to GPU
+    heatmap = heatmap.to(device)
+    
     # Create tensors for averaging with shifted versions of the heatmap tensor
     shifted_left    = torch.roll(heatmap, shifts=1)
     shifted_right   = torch.roll(heatmap, shifts=-1)
     shifted_up      = torch.roll(heatmap, shifts=1, dims=0)
     shifted_down    = torch.roll(heatmap, shifts=-1, dims=0)
     
-    # Set certain edges to zero so numbers dont wrap around
+    # Set certain edges to zero so numbers don't wrap around
     shifted_left[:, -1] = 0
     shifted_right[:, 0] = 0
     shifted_up[-1, :] = 0
@@ -64,24 +69,23 @@ def runtimestep(heatmap: torch.Tensor, time_counter):
     padded_shifted_down  = torch.nn.functional.pad(shifted_down, (1, 1, 1, 1), mode='constant', value=0)
     
     # Calculate the average using tensors and avoid iteration
-    # average the 3x3 square around each element
+    # Average the 3x3 square around each element
     avg = (padded_heatmap + padded_shifted_left + padded_shifted_right + padded_shifted_up + padded_shifted_down) / 5
     
-    # remove padding
+    # Remove padding
     avg = avg[1:-1, 1:-1]
     
-    #print_str_representation(avg, time_counter)
-    # f.write(f"{','.join([format(val, '.3f') for val in _aux_tensor.tolist()])}\n")
-
+    # Move the result back to CPU if needed
+    avg = avg.cpu()
+    
     return avg
+
 
 
 def get_color_escape(r, g, b, background=False):
     return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
 
 def get_rgb_ndarr(arr: torch.Tensor) -> np.ndarray:
-    device = arr.device
-
     scaled_vals = -0.693 * arr + 0.693
     hsv_tensor = torch.stack([scaled_vals, torch.ones_like(arr), torch.ones_like(arr)], dim=-1)
     hsv_array = hsv_tensor.cpu().numpy()
